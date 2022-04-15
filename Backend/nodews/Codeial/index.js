3a7799c7a44ff40f5a67d05a550338773e0d820e
KARANAM AJAY NAG <ajaynagkaranam@gmail.com>
2022-04-15 18:05:31 +0530

    layout partials

--- Backend/nodews/Codeial/index.js
+++ Backend/nodews/Codeial/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
 
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+app.use(express.static('./assets'));
 
 //use express router
 app.use('/', require('./routes'));
