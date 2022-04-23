ec9b6150530216195304072d11f47753f103d969
KARANAM AJAY NAG <ajaynagkaranam@gmail.com>
2022-04-23 18:48:25 +0530

    sign in and sign up

--- Backend/nodews/Codeial/controllers/users_controller.js
+++ Backend/nodews/Codeial/controllers/users_controller.js
@@ -3,3 +3,23 @@ module.exports.profile = (req, res) => {
     user: 'Ajay'
   });
 } 
+
+module.exports.signUp = (req, res) => {
+  return res.render('user_sign_up',{
+    title:  "Codeial | SignUp"
+  });
+};
+//
+module.exports.signIn = (req, res) => {
+  return res.render('user_sign_in',{
+    title:  "Codeial | SignIn"
+  });
+};
+
+//get the sign up data
+module.exports.create = (req, res) => {
+};
+
+//sign in and create a session for the user
+module.exports.createSession = (req, res) => {
+}
